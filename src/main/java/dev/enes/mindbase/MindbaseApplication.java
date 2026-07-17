de6a2be2f611187c9eefb5cc9ad4e287954ea276
Enes Incekara <enesincekara61@gmail.com>
2026-07-17 17:14:50 +0300

    build: kurumsal proje altyapisi, .env izolasyonu ve pgvector entegrasyonu kuruldu

---
+++ src/main/java/dev/enes/mindbase/MindbaseApplication.java
@@ -0,0 +1,13 @@
+package dev.enes.mindbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/enes/mindbase/MindbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enes.mindbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
